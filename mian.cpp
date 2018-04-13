9b1f599d624dbd0c861f682b23c53fc1e56a433c
0obriano0 <37872600+0obriano0@users.noreply.github.com>
2018-04-14 00:06:02 +0800

    版面優化
    界面優化
    外加可以調速度

--- mian.cpp
+++ mian.cpp
@@ -4,15 +4,18 @@
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
+#include <process.h> //多執行續函式庫
 
 using namespace std;
 
 void OpenFile(char *st);
 void gotoxy(int x, int  y);
+void SetConsoleSize(int w,int h);
+void SetCursorVisible(BOOL Visible, DWORD Size);
 
-int main(){
+void anime_show(int *a){
 	int Num1 = 1;
-	while(1){
+	while(1){		
 		char st[100]="data\\image_";
 		char buffer[33];
 		itoa (Num1,buffer,10);
@@ -25,11 +28,38 @@ int main(){
 		Num1++;
 		if(Num1 >= 10)
 			Num1 = 1;
-		Sleep(100);
+		Sleep(*a);
+	}
+}
+
+int main(){
+	int Num1 = 50;
+	SetConsoleSize(650,900);
+	SetCursorVisible(false,1);
+	_beginthread(anime_show, 0, &Num1);
+	
+	int key1,key2;
+	while(1){
+		if (!kbhit())
+			key1= getch();
+			
+		if (key1 == 0 || key1==224){
+			key2 = getch();
+			switch(key2){
+				case 72:
+					if(Num1 <= 1500)
+						Num1+=50;
+				break;
+				case 80:
+					if(Num1 > 0)
+						Num1-=50;
+				break;
+			}
+		}else{
+		}
 	}
 	return 0;
 }
-
 
 void OpenFile(char *st){
 	char ch;
@@ -44,9 +74,23 @@ void OpenFile(char *st){
 	fclose(fp);  //關檔 
 }
 
-void gotoxy(int x, int  y)
-{
+void gotoxy(int x, int  y){
         COORD point;
         point.X = x, point.Y = y;
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), point);
+}
+
+void SetConsoleSize(int w,int h){
+	HWND console = GetConsoleWindow();
+    RECT r;
+    GetWindowRect(console, &r); //stores the console's current dimensions
+
+    MoveWindow(console, r.left, r.top, w, h, TRUE);
+}
+
+void SetCursorVisible(BOOL Visible, DWORD Size){
+        CONSOLE_CURSOR_INFO cci;
+        cci.bVisible = Visible; // 是否可視
+        cci.dwSize = Size; // 設定大小,1~100
+        SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cci);
 }
